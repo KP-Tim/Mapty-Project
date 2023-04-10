326d4e650a3d2a5ad77e73fa6792ad5f9111e7ea
KP-Tim <kancanha@gmail.com>
2023-04-10 10:46:32 -0700

    Completed Lazy Loading Images Lec:199

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -95,13 +95,8 @@ nav.addEventListener('mouseover', handleover.bind(0.5));
 
 nav.addEventListener('mouseout', handleover.bind(1));
 
-const navHeight = nav.getBoundingClientRect().height;
-console.log(navHeight);
-
 const sticky = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry);
-
   if (!entry.isIntersecting) {
     nav.classList.add('sticky');
   } else {
@@ -109,18 +104,113 @@ const sticky = function (entries, observer) {
   }
 };
 
-const obsOptions = {
-  root: null,
-  threshold: 0,
-};
+const navHeight = nav.getBoundingClientRect().height;
 
-const headerObserver = new IntersectionObserver(sticky, {
+const headerOptions = {
   root: null,
   threshold: 0,
   rootMargin: `-${navHeight}px`,
-});
+};
+
+const allSections = document.querySelectorAll('.section');
+
+const headerObserver = new IntersectionObserver(sticky, headerOptions);
 
 headerObserver.observe(header);
+
+//Reveal sections
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  // Make it stop observe
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
+// Lazy loading images
+// imgTargets, imgObserver, loadImg
+
+const imgTargets = document.querySelectorAll('img[data-src');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) return;
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+  // Make it stop observe
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(function (img) {
+  imgObserver.observe(img);
+});
+
+/*
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+/*
+
+// const navHeight = nav.getBoundingClientRect().height;
+// console.log(navHeight);
+
+// const sticky = function (entries, observer) {
+//   const [entry] = entries;
+//   console.log(entry);
+
+//   if (!entry.isIntersecting) {
+//     nav.classList.add('sticky');
+//   } else {
+//     nav.classList.remove('sticky');
+//   }
+// };
+
+// const obsOptions = {
+//   root: null,
+//   threshold: 0,
+// };
+
+// const headerObserver = new IntersectionObserver(sticky, {
+//   root: null,
+//   threshold: 0,
+//   rootMargin: `-${navHeight}px`,
+// });
+
+// headerObserver.observe(header);
 
 // const obsCallback = function (entries, observer) {
 //   entries.forEach(entry => console.log(entry));
