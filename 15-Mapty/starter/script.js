60b0c157efa6654a4357766ab653b53473564764
KP-Tim <kancanha@gmail.com>
2023-05-16 15:51:34 -0700

    Revert "Displaying a Map Marker"
    This reverts commit 52edb14fba5d5fb2c4f86b53ea94c69c27c9eb72.

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -42,19 +42,7 @@ if (navigator.geolocation.getCurrentPosition) {
         console.log(mapEvent);
         const { lat, lng } = mapEvent.latlng;
         console.log(lat, lng);
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 250,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: 'running-popup',
-            })
-          )
-          .setPopupContent('Workout')
-          .openPopup();
+        L.marker([lat, lng]).addTo(map).bindPopup('Workout').openPopup();
       });
     },
     function () {
