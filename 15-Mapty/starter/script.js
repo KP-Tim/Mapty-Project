f171c85b587a1645a051b32437c025ab8cd22d4e
KP-Tim <kancanha@gmail.com>
2023-05-16 15:10:59 -0700

    Added pin to clicked location

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -12,4 +12,53 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 // Start new project
-// This will be on the new branch called test
+
+// Get current geo location
+if (navigator.geolocation.getCurrentPosition) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(position);
+      console.log(latitude);
+      console.log(longitude);
+
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13);
+      // console.log(map);
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+        console.log(lat, lng);
+        L.marker([lat, lng]).addTo(map).bindPopup('Workout').openPopup();
+      });
+    },
+    function () {
+      alert('Can not get the current location');
+    }
+  );
+}
+console.log(map);
+//https://www.google.com/maps/@34.000888,-118.2021674,15z
+
+// map.event.addListener(map, 'click', function (e) {
+//   placeMarker(e.latLng);
+// });
+// function placeMarker(location) {
+//   var marker = new google.maps.Marker({
+//     position: location,
+//     map: map,
+//   });
+// }
