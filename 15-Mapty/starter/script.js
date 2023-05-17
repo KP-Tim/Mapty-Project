e219c682181efd2e00b4855c065dae1b4fe86fa7
KP-Tim <kancanha@gmail.com>
2023-05-17 14:59:35 -0700

    Adding Pin On The Map

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -12,3 +12,45 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 // Start new project
+if (navigator.geolocation.getCurrentPosition) {
+  navigator.geolocation.getCurrentPosition(
+    function (pos) {
+      const { latitude, longitude } = pos.coords;
+      console.log(pos);
+      console.log(latitude, longitude);
+      console.log(`https://www.google.com/maps/@${latitude},${longitude},15z`);
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }).addTo(map);
+
+      L.marker([51.5, -0.09])
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
+
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+        console.log(lat, lng);
+        L.marker([lat, lng]).addTo(map).bindPopup('Workout').openPopup();
+      });
+    },
+    function () {
+      alert('Can not find your location!');
+    }
+  );
+}
+// const map = L.map('map').setView([51.505, -0.09], 13);
+
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//   maxZoom: 19,
+//   attribution:
+//     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+// }).addTo(map);
